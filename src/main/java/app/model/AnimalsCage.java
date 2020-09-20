08bc5c5f7aab6cc424837f1fe4dc25a558cf9a74
jm-program <49599651+jm-program@users.noreply.github.com>
2020-09-20 14:08:48 +0300

    Delete Dog.java
    Согласование с условием задачи

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,33 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Animal animal2;
+    private Timer time;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("cat") Animal animal, @Qualifier("dog") Animal animal2, Timer time) {
+        this.animal = animal;
+        this.animal2 = animal2;
+        this.time = time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animal.toString() + ", " + animal2.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
     }
 }
